ed1902daf1076486c26ea2c98f2da8aa9b0a3d4f
m4r4v <jmarquezrave@gmail.com>
2023-08-18 19:27:00 -0400

    added keyboard interruption message

--- main.py
+++ main.py
@@ -5,19 +5,24 @@ from termcolor import colored
 
 
 #Title
-print(colored('>> Type the website address to get inforation from <<', 'yellow', attrs=['bold', 'underline']))
+print(colored('\n>> Type the website address to get inforation from <<', 'yellow', attrs=['bold', 'underline']))
 #subtitle
 print(colored('example: google.com', 'dark_grey'))
 
-input = input('Domain: https://')
+try:
+    while True:
+        input = input('Domain: https://')
 
-req = requests.get('https://' + input)
+        req = requests.get('https://' + input)
 
-ip = socket.gethostbyname(input)
+        ip = socket.gethostbyname(input)
 
-print('\n' + colored('IP address: ', 'light_yellow') + colored(ip, 'green'))
+        print('\n' + colored('IP address: ', 'light_yellow') + colored(ip, 'green'))
 
-for key, value in req.headers.items():
-    print(colored(key, 'light_yellow') + ": " + colored(value, 'green'))
+        for key, value in req.headers.items():
+            print(colored(key, 'light_yellow') + ": " + colored(value, 'green'))
 
+        pass
+except KeyboardInterrupt:
+    print("\n" + colored('Process interrupted, try again', 'red') + "\n")
 # print(colored(req.headers, 'light_green'))
